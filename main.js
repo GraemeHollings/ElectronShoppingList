a8a31d9a616cd537326d66d89709534e5da763f3
Graeme <gdhollingsworth93@gmail.com>
2019-11-04 22:23:52 -0500

    Intial Commit.

---
+++ main.js
@@ -0,0 +1,69 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+//Bringing everything in from Electron
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function()
+{
+    //Create new window
+    mainWindow = new BrowserWindow({});
+    //Load HTML file into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    })); //file://dirname//mainWindow.html
+
+    //Build Menu from template
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    //Insert the menu
+    Menu.setApplicationMenu(mainMenu)
+});
+
+//Handle creating a new item window
+function createAddWindow()
+{
+
+    
+}
+
+
+
+
+//Create a menu template
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: 
+        [
+            {
+                label: 'Add Item',
+                click()
+                {
+                    createAddWindow();
+                }
+
+            },
+            {
+                label: 'Clear Items'
+
+            },
+            {
+
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 
+                'CTRL+Q',
+                click()
+                {
+                    app.quit();
+                }
+            }
+        ]
+
+    }
+];
